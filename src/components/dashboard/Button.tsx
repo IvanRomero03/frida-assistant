29f108f2b352fa09b2b3f33e41b3590cdde9cefb
Alejandra Coeto <a01285221@tec.mx>
2023-09-23 15:06:26 -0600

    buttons

--- src/components/dashboard/Button.tsx
+++ src/components/dashboard/Button.tsx
@@ -3,11 +3,11 @@ interface buttonProps {
     icon: any;
 }
 
-const Button:React.FC<buttonProps> = ({label, icon:Icon}) => {
+const Button: React.FC<buttonProps> = ({ label, icon: Icon }) => {
     return (
-        <div className='flex bg-white px-6 py-3 w-48'>
-                <Icon className="bg-blue-400"/>
-               {label}
+        <div className='flex bg-white px-6 py-3 w-48 rounded-md'>
+            <Icon className="text-blue-400 font-bold self-center mr-2" />
+            {label}
         </div>
     )
 }
--- src/components/dashboard/mainButtons.tsx
+++ src/components/dashboard/mainButtons.tsx
@@ -1,19 +1,13 @@
 import { SlDocs } from 'react-icons/sl'
 import Button from './Button'
+import { BsChatRight } from 'react-icons/bs'
 
 const MainButtons = () => {
     return (
         <div className="fixed flex w-full ml-52 mt-5 gap-3">
-            
-            <Button label="My documents" icon = {SlDocs} />
 
-            
-
-            <div className='flex bg-white px-6 py-3 w-48'>
-                <SlDocs />
-                Chatbot
-
-            </div>
+            <Button label="My documents" icon={SlDocs} />
+            <Button label="Chat Bot" icon={BsChatRight} />
 
         </div>
     )
