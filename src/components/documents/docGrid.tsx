343335f4ba612cb489ac7a4db4850577158c7ca6
Alejandra Coeto <a01285221@tec.mx>
2023-09-24 07:22:55 -0600

    Docs update

--- src/components/documents/docGrid.tsx
+++ src/components/documents/docGrid.tsx
@@ -4,8 +4,7 @@ import { useSession } from "next-auth/react";
 
 
 const DocGrid = () => {
-    const {data} = useSession();
-
+    const { data } = useSession();
     const { data: docs } = api.docs.getAll.useQuery({
         id: data?.user.id || ""
     });
@@ -13,7 +12,7 @@ const DocGrid = () => {
     return (
         <div className=" grid gap-5 mt-8 w-auto grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 overflow-y-scroll">
             {docs?.map((doc, key) => (
-                <DocCard key={key}  id={doc.id} summary={doc.text} title={doc.name}/>
+                <DocCard key={key} id={doc.id} summary={doc.text} title={doc.name} />
             ))}
         </div>
     )
