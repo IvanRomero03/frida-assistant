de605b84a159fd9b5fa94ec9d23c0b401f3d627b
Ale-Coeto <109093534+Ale-Coeto@users.noreply.github.com>
2023-09-23 19:56:38 -0600

    Revert "Dashboard"

--- src/components/dashboard/Button.tsx
+++ src/components/dashboard/Button.tsx
@@ -1,16 +1,12 @@
-import clsx from "clsx";
-
 interface buttonProps {
     label: string;
     icon: any;
-    selected: boolean;
-    onClick: () => void;
 }
 
-const Button: React.FC<buttonProps> = ({ label, icon: Icon, selected, onClick }) => {
+const Button: React.FC<buttonProps> = ({ label, icon: Icon }) => {
     return (
-        <div className={clsx(selected ? 'bg-gray-100' : 'bg-white text-gray-500', 'flex px-6 py-3 w-48 rounded-md justify-center')} onClick={onClick}>
-            <Icon className="text-blue-400 font-bold self-center mr-2" onClick={onClick} />
+        <div className='flex bg-white px-6 py-3 w-48 rounded-md'>
+            <Icon className="text-blue-400 font-bold self-center mr-2" />
             {label}
         </div>
     )
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,31 +1,13 @@
 import { NextPage } from "next"
-import Chat from "rbrgs/components/chat/chat";
 import MainButtons from "rbrgs/components/dashboard/mainButtons"
 import PromptHistory from "rbrgs/components/dashboard/promptHistory"
-import DocGrid from "rbrgs/components/documents/docGrid";
-import Documents from "rbrgs/components/documents/documents";
-import { useCallback, useState } from "react";
-
-type variant = "DOCS" | "CHAT"
 
 const Dashboard: NextPage = () => {
-    const [variant, setSelected] = useState("DOCS");
-
-    const toggleVariant = useCallback(() => {
-        setSelected(variant === 'DOCS' ? 'CHAT' : 'DOCS')
-    }, [variant])
-
     return (
         <>
-            <MainButtons variant={variant} onClick={toggleVariant} />
-
-            <div className="bg-gray-100 h-screen w-full flex overflow-hidden">
+            <div className="bg-gray-100 h-screen flex">
                 <PromptHistory />
-
-                <div className="h-full pl-10 mt-32 w-full">
-                    {variant == 'DOCS' ? <Documents /> : <Chat />}
-                </div>
-
+                <MainButtons />
             </div>
         </>
     )
